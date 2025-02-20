d14703b803165cb4807bebd104f8dbc51210b657
FASEISE <128413054+FASEISE@users.noreply.github.com>
2025-02-20 16:42:58 +0100

    Update stores.php

--- model/stores.php
+++ model/stores.php
@@ -1,1 +1,37 @@
+<?php
 
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+class Store{
+    private $table;
+    private $conection;
+    
+    public function __construct() {
+        
+    }
+    public function insertIntoTable() {
+        $cod;
+        $name;
+        $tlf;
+        $sql = "INSERT INTO store (cod, name, tlf) VALUES" .
+                            "($cod, '$name', '$tlf')";
+        
+        return $sql;
+    }
+    
+    public function updateTable(){
+         $cod;
+        $name;
+        $tlf;
+        $sql ="UPDATE store SET name='$name', tlf='$tlf' WHERE cod='$cod'";
+        
+        return $sql;
+    }
+    
+    public function deleteFromTable(){
+        
+    }
+}
