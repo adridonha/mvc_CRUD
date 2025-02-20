9532d87fd3b837d39a61dc1e16b76554af160dbf
adridonha <161077454+adridonha@users.noreply.github.com>
2025-02-20 17:03:20 +0100

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Database connection values */
+define("DB_HOST", "localhost");
+define("DB", "mvc_example");
+define("DB_USER", "dwes");
+define("DB_PASS", "dwes");
+
+/* Default options
+define("DEFAULT_CONTROLLER", "note");
+define("DEFAULT_ACTION", "list");
+ */
+
+?>
