1e8b7bc9f4a2bc4edd9f73cbf81558ed4f0f28b9
alvarotrig <161077435+alvarotrig@users.noreply.github.com>
2025-02-24 19:31:25 +0100

    Create controlerDefault.php

---
+++ controller/controlerDefault.php
@@ -0,0 +1,9 @@
+<?php
+
+class ControlerDefault {
+    public static function mostrarBotonesHeader() {
+        // Cargar la vista de botonesHeader desde la carpeta view
+        include 'view/botonesHeader.php'; // Asegúrate de que la ruta sea correcta
+    }
+}
+?>
