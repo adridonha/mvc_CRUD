839817304ffdbcec1f0a7cba1c80987ccc33cee9
alvarotrig <161077435+alvarotrig@users.noreply.github.com>
2025-02-24 18:29:29 +0100

    Create botonesHeader.php

---
+++ view/botonesHeader.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Navigation</title>
+
+</head>
+<body>
+    <div class="btn-container">
+        <button class="btn">Products</button>
+        <button class="btn">Families</button>
+        <button class="btn">Stocks</button>
+        <button class="btn">Stores</button>
+    </div>
+</body>
+</html>
