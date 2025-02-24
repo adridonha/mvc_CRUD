fb2bd3f585d4b7e3b4eebc5ff878f9bb72349e6e
alvarotrig <161077435+alvarotrig@users.noreply.github.com>
2025-02-24 19:34:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+// index.php
+
+// Incluir el controlador
+require_once 'controller/controlerDefault.php';
+
+// Llamar al método para mostrar la vista
+ControlerDefault::mostrarBotonesHeader();
+?>
