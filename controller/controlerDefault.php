cc2744b952f5ef7a190cf0e53bcd0340ff3fd442
Abeloc <161881978+Abeloc@users.noreply.github.com>
2025-03-06 16:43:32 +0100

    Delete controller directory

--- controller/controlerDefault.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ControlerDefault {
-    public static function mostrarBotonesHeader() {
-        // Cargar la vista de botonesHeader desde la carpeta view
-        include 'view/botonesHeader.php'; // Asegúrate de que la ruta sea correcta
-    }
-}
-?>
