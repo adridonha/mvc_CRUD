eaee00e943109ee2ba42121b284f39767eaa44ad
Abeloc <161881978+Abeloc@users.noreply.github.com>
2025-03-06 16:43:56 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// index.php
-
-// Incluir el controlador
-require_once 'controller/controlerDefault.php';
-
-// Llamar al método para mostrar la vista
-ControlerDefault::mostrarBotonesHeader();
-?>
