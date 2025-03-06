c1c97987948f9bbee51f94c57ea5f9397012a6b0
Abeloc <161881978+Abeloc@users.noreply.github.com>
2025-03-06 16:43:39 +0100

    Delete model directory

--- model/header.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function showHeader() {
-echo '<input type="button" name="products" value="Products" onclick="window.location =\'products.php\'"/>';
-echo '<input type="button" name="families" value="Families" onclick="window.location =\'families.php\'"/>';
-echo '<input type="button" name="Stock" value="Stocks" onclick=" window.location = \'stock.php\'"/>';
-echo '<input type="button" name="Stores" value="Stores" onclick=" window.location = \'stores.php\'"/>';
-}
-
